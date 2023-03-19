cc4b3d1d1f7830dd774024af8e59eb2a24d4cfc8
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-03-18 23:40:35 -0500

    Update adil.c

--- adil.c
+++ adil.c
@@ -13,9 +13,10 @@ int flag=0;
 int main(int argc,char* argv[])
 {
     int i,k,n;
+    int attr[10000];
     n=atoi(argv[1]);
     //printf("%d\n",n);
-    k=n/2;
+    k = (int) (n/2);
     //printf("%d\n",n);
 
     if(argc==2)
@@ -23,7 +24,35 @@ int main(int argc,char* argv[])
         //printf("okay\n");
         if(n>0 && n<15)
         {
-            printf("okay2\n");
+            //printf("okay2\n");
+            for(i=0;i<k;i++)
+            {
+                for(i = 0; i < k; i++)
+                    {
+                        //printf("%d\n",i);
+                        pthread_create(&readers[i], &attr[0], reader_thread,(void*) i);
+                    }
+
+                    for(i = 0; i < k; i++)
+                    {
+                        //printf("%d\n",i);
+                        pthread_join(&readers[i],NULL);
+                    }
+
+                    pthread_create(&writer[0], &attr[0], writer_thread,NULL);
+                    pthread_join(&writer[0],NULL);
+
+                    for(i = k ; i < n ; i++)
+                        {
+                            pthread_create(&readers[i], &attr[0], reader_thread,(void*) i);
+                        }
+                    for(i = k ; i < n ; i++)
+                        {
+                            pthread_join(&readers[i], NULL);
+                        }
+
+
+            }
         }
         else{
             printf("invalid : n should be more than 0 and less than 15\n");
