4c228d412c2bb734a16d9c25bff5bbbab2af62d4
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-03-18 23:04:52 -0500

    Update new.c

--- new.c
+++ new.c
@@ -1,106 +1,96 @@
-#include<stdio.h>
-#include<semaphore.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int nmax=14;
-int Readmax=250000000;
+#define MAX_READERS 14 // Maximum number of readers allowed
+#define TOTAL_READS 250000000 // Total number of times each reader will read the shared counter
 
-int counter=0;
-int wflag=0;
-sem_t mutex,wrt;
+/* Global variables */
+int shared_counter = 0; // Shared counter
+int in_cs = 0; // Flag indicating if a writer is in critical section
+sem_t mutex, wrt; // Semaphore for mutual exclusion and one for writer preference
 
-
-void* RS()
-{
+/* Writer thread function */
+void* writer_thread(void* arg) {
     int i;
-    for(i=0;i<250000000;i++)
-    {
-        i=i;
+    for (i = 0; i < 25000; i++) {
+        sem_wait(&wrt); // Wait for writer preference
+        in_cs = 1; // Set flag to indicate writer is in critical section
+        // Critical section
+        shared_counter += 1;
+        // End of critical section
+        in_cs = 0; // Reset flag
+        sem_post(&wrt); // Release writer preference
     }
+    printf("Writer done.\n");
+    pthread_exit(NULL);
 }
 
-void* readerfunc(void* arg)
-{
-    int *x=(int *) arg;
-    int y=*x;
-    /*for(i=0;i<Readmax;i++)
-    {
-        sem_wait(&mutex);
-        if(wflag==1)
-    }
-    */
-    printf("readerfunc okay : %d\n",y);
-}
-
-void* writerfunc(void* arg)
-{
-    int *x=(int *) arg;
-    int y=*x;
-    printf("writerfunc okay : %d\n",y);
-}
-
-
-
-
-
-int main(int argc, char* argv[])
-{
-    int n=atoi(argv[1]),i;
-    int k=n/2;
-    int a[14];
-
-    printf("c=%d n=%d\n",argc,n);
-
-    sem_init(&mutex,0,1);
-    sem_init(&wrt,0,1);
-    pthread_t R[n], writer;
-
-
-
-
-    if(argc==2){
-        //printf("\ngood\n");
-        if(n>0 && n<15)
-        {
-            //printf("\nn is okay\n");
-            
-            for(i=0;i<k;i++)
-            {
-                a[i]=i;
-                pthread_create(&R[i],NULL,readerfunc,(void *) &a[i]);
-            }
-
-            
-            pthread_create(&writer,NULL,writerfunc,NULL);
-
-            sleep(4);
-            for(i=k;i<n;i++)
-            {
-                a[i]=i;
-                pthread_create(&R[i],NULL,readerfunc,(void *) &a[i]);
-            }
-            
-
-            for(i=0;i<n;i++)
-            {
-                pthread_join(&R[i],NULL);
-            }
-            pthread_join(&writer,NULL);
-
-            sem_destroy(&mutex);
-            sem_destroy(&wrt);
-
-
+/* Reader thread function */
+void* reader_thread(void* arg) {
+    int reader_id = (int) arg;
+    int i;
+    for (i = 0; i < TOTAL_READS; i++) {
+        sem_wait(&mutex); // Wait for mutual exclusion
+        if (in_cs == 1) { // Check if writer is in critical section
+            printf("Error: Reader %d found writer in critical section.\n", reader_id);
+            exit(1);
         }
-        else{
-            printf("\natleast 1 reader or max of 14 readers\n");
-        }
+        // Critical section
+        int temp_counter = shared_counter; // Read shared counter
+        // End of critical section
+        sem_post(&mutex); // Release mutual exclusion
+        relax_and_spend_time(); // Spend some time outside the critical section
     }
-    else
-    {
-        printf("\ninvalid arguments\n");
-        exit(0);
+    printf("Reader %d done.\n", reader_id);
+    pthread_exit(NULL);
+}
+
+/* Function to spend some time outside the critical section */
+void relax_and_spend_time() {
+    int i;
+    for (i = 0; i < 250; i++) { // Can be adjusted to control how much time to spend outside the critical section
+        i = i; // Dummy operation to spend some time
     }
+}
+
+/* Main function */
+int main(int argc, char* argv[]) {
+    if (argc != 2) { // Check if number of arguments is correct
+        printf("Usage: ./program_name number_of_readers\n");
+        exit(1);
+    }
+    int num_of_readers = atoi(argv[1]);
+    if (num_of_readers < 1 || num_of_readers > MAX_READERS) { // Check if number of readers is within range
+        printf("Error: Number of readers should be between 1 and %d.\n", MAX_READERS);
+        exit(1);
+    }
+
+    /* Initialize semaphores */
+    sem_init(&mutex, 0, 1);
+    sem_init(&wrt, 0, 1);
+
+    /* Create threads */
+    pthread_t readers[num_of_readers], writer;
+    int i, k = num_of_readers / 2;
+    for (i = 0; i < k; i++) {
+        pthread_create(&readers[i], NULL, reader_thread, (void*) i);
+    }
+    pthread_create(&writer, NULL, writer_thread, NULL);
+    for (i = k; i < num_of_readers; i++) {
+        pthread_create(&readers[i], NULL, reader_thread, (void*) i);
+    }
+
+    /* Join threads */
+ for (i = 0; i < num_of_readers; i++) {
+        pthread_join(readers[i], NULL);
+    }
+    pthread_join(writer, NULL);
+
+    /* Destroy semaphores */
+    sem_destroy(&mutex);
+    sem_destroy(&wrt);
+
     return 0;
 }
